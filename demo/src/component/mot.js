3b5adeb5553afdc4c2154df27758de35611c6b03
joker1389 <68012029+joker1389@users.noreply.github.com>
2020-07-09 11:10:10 +0700

    git add.

---
+++ demo/src/component/mot.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class mot extends Component{
+  render(){
+    return(
+      <div>	
+      	<h1>aaaaa</h1>
+      </div>
+    );
+  }
+}
+
+export default mot;
